80fe8bb8c55f4a8278da17168bd6f1945f00a92b
Artur Gniewowski <artur2113@o2.pl>
2019-10-08 22:26:28 +0200

    Refactor: java db to kotlin db

---
+++ app/src/main/java/com/artf/shoppinglist/database/ShoppingListDatabase.kt
@@ -0,0 +1,37 @@
+package com.artf.shoppinglist.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [ShoppingList::class, Product::class], version = 2, exportSchema = false)
+abstract class ShoppingListDatabase : RoomDatabase() {
+
+    abstract val shoppingListDatabaseDao: ShoppingListDatabaseDao
+
+    companion object {
+
+        @Volatile
+        private var INSTANCE: ShoppingListDatabase? = null
+
+        fun getInstance(context: Context): ShoppingListDatabase {
+            synchronized(this) {
+                var instance = INSTANCE
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        ShoppingListDatabase::class.java,
+                        "shopping_list_database"
+                    )
+                        .fallbackToDestructiveMigration()
+                        .build()
+
+                    INSTANCE = instance
+                }
+
+                return instance
+            }
+        }
+    }
+}
--- app/src/main/java/com/artf/shoppinglist/di/AppModule.kt
+++ app/src/main/java/com/artf/shoppinglist/di/AppModule.kt
@@ -1,8 +1,8 @@
 package com.artf.shoppinglist.di
 
 import android.content.Context
+import com.artf.shoppinglist.database.ShoppingListDatabase
 import com.artf.shoppinglist.database.ShoppingListDatabaseDao
-import com.artf.shoppinglist.database.ShoppingListDatabaseJava
 import com.artf.shoppinglist.di.viewModel.ViewModelModule
 import com.artf.shoppinglist.repository.ShoppingListRepository
 import com.artf.shoppinglist.repository.ShoppingListRepositoryInt
@@ -19,7 +19,7 @@ object AppModule {
     @Singleton
     @Provides
     fun provideDataBase(context: Context): ShoppingListDatabaseDao {
-        return ShoppingListDatabaseJava.getInstance(context).shoppingListDatabaseDao()
+        return ShoppingListDatabase.getInstance(context).shoppingListDatabaseDao
     }
 
     @JvmStatic
